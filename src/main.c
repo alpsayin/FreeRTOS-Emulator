bdd07dc68d80631a6f145029a98e2ca80ea942db
Alp Sayin <alpsayin@gmail.com>
2020-08-17 18:45:00 +0300

    more task, semaphore and timer examples

--- src/main.c
+++ src/main.c
@@ -31,6 +31,11 @@
 
 static TaskHandle_t DemoTask = NULL;
 static TaskHandle_t FakeKeyISRTask = NULL;
+static TaskHandle_t myTask = NULL;
+static TaskHandle_t myTask2 = NULL;
+static TaskHandle_t pingTask = NULL;
+static TaskHandle_t pongTask = NULL;
+SemaphoreHandle_t ppSem;
 
 typedef struct buttons_buffer {
     unsigned char buttons[SDL_NUM_SCANCODES];
@@ -42,8 +47,11 @@ static buttons_buffer_t buttons = { 0 };
 void xGetButtonInput(void)
 {
     if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+    	//anyone who attempts to take the SEM now will BLOCK
         xQueueReceive(buttonInputQueue, &buttons.buttons, 0);
+    	//anyone who attempts to take the SEM now will BLOCK
         xSemaphoreGive(buttons.lock);
+    	//anyone who attempts or who attempted to take the SEM now will continue
     }
 }
 
@@ -52,6 +60,48 @@ void xGetButtonInput(void)
 void fakeKeyISR(char keycode)
 {
     printf("fake ISR fired: %c \r\n", keycode);
+}
+
+void vMyTask(void *pvParameters)
+{
+	int mycounter = 0;
+	char* taskname = pcTaskGetName(xTaskGetCurrentTaskHandle());
+	while (1) 
+    {
+        printf("%s task: %d\r\n", taskname, mycounter++);
+        // Basic sleep of 1000 milliseconds
+        vTaskDelay((TickType_t)500);
+    }
+}
+
+void count_things(int new_errs, int* cerrs)
+{
+	int temp_errs = *cerrs;
+	*cerrs = temp_errs + new_errs;
+	printf("new thing count is %d\n", *cerrs);
+}
+
+
+long long int pingpong = 0;
+void vPingPongTask(void *pvParameters)
+{
+	int cerrs = 0;
+	char* taskname = pcTaskGetName(xTaskGetCurrentTaskHandle());
+	while (1) 
+    {
+	    if (xSemaphoreTake(ppSem, 0) == pdTRUE) {
+	    	printf("%s task: with task_param: %s, %lld\r\n", taskname, (char*)pvParameters, pingpong);
+	        // Basic sleep of 1000 milliseconds
+	        pingpong++;
+	        if(pingpong % 2)
+	        {
+	        	count_things(1, &cerrs);
+	        }
+        	vTaskDelay((TickType_t)1000);
+	        xSemaphoreGive(ppSem);
+	    }
+        vTaskDelay((TickType_t)100);
+    }
 }
 
 void vFakeKeyISRFireTask(void *pvParameters)
@@ -141,10 +191,42 @@ void vDemoTask(void *pvParameters)
         tumDrawUpdateScreen(); // Refresh the screen to draw string
 
         // Basic sleep of 1000 milliseconds
-        vTaskDelay((TickType_t)100);
+        vTaskDelay((TickType_t)10);
     }
 }
+void vTimerCallback( TimerHandle_t xTimer )
+ {
+ const uint32_t ulMaxExpiryCountBeforeStopping = 10;
+ uint32_t ulCount;
 
+    /* Optionally do something if the pxTimer parameter is NULL. */
+    configASSERT( xTimer );
+
+    /* The number of times this timer has expired is saved as the
+    timer's ID.  Obtain the count. */
+    ulCount = ( uint32_t ) pvTimerGetTimerID( xTimer );
+
+    /* Increment the count, then test to see if the timer has expired
+    ulMaxExpiryCountBeforeStopping yet. */
+    printf("We're inside the vTimerCallback with %d\n", ulCount);
+    ulCount++;
+
+    /* If the timer has expired 10 times then stop it from running. */
+    if( ulCount >= ulMaxExpiryCountBeforeStopping )
+    {
+        /* Do not use a block time if calling a timer API function
+        from a timer callback function, as doing so could cause a
+        deadlock! */
+        xTimerStop( xTimer, 0 );
+    }
+    else
+    {
+       /* Store the incremented count back into the timer's ID field
+       so it can be read back again the next time this software timer
+       expires. */
+       vTimerSetTimerID( xTimer, ( void * ) ulCount );
+    }
+ }
 int main(int argc, char *argv[])
 {
     char *bin_folder_path = tumUtilGetBinFolderPath(argv[0]);
@@ -166,11 +248,52 @@ int main(int argc, char *argv[])
         goto err_init_audio;
     }
 
+    ppSem = xSemaphoreCreateMutex(); // Locking mechanism
+
     buttons.lock = xSemaphoreCreateMutex(); // Locking mechanism
     if (!buttons.lock) {
         PRINT_ERROR("Failed to create buttons lock");
         goto err_buttons_lock;
     }
+
+    int x;
+    for( x = 0; x < NUM_TIMERS; x++ )
+     {
+         xTimers[ x ] = xTimerCreate
+                   ( /* Just a text name, not used by the RTOS
+                     kernel. */
+                     "Timer",
+                     /* The timer period in ticks, must be
+                     greater than 0. */
+                     (TickType_t) ((x+1)*1000*portTICK_PERIOD_MS),
+                     /* The timers will auto-reload themselves
+                     when they expire. */
+                     pdTRUE,
+                     /* The ID is used to store a count of the
+                     number of times the timer has expired, which
+                     is initialised to 0. */
+                     ( void * ) 0,
+                     /* Each timer calls the same callback when
+                     it expires. */
+                     vTimerCallback
+                   );
+
+         if( xTimers[ x ] == NULL )
+         {
+             /* The timer was not created. */
+         }
+         else
+         {
+             /* Start the timer.  No block time is specified, and
+             even if one was it would be ignored because the RTOS
+             scheduler has not yet been started. */
+             if( xTimerStart( xTimers[ x ], 0 ) != pdPASS )
+             {
+                 /* The timer could not be set into the Active
+                 state. */
+             }
+         }
+     }
 
     if (xTaskCreate(vDemoTask, "DemoTask", mainGENERIC_STACK_SIZE * 2, NULL,
                     mainGENERIC_PRIORITY, &DemoTask) != pdPASS) {
@@ -179,6 +302,26 @@ int main(int argc, char *argv[])
 
     if (xTaskCreate(vFakeKeyISRFireTask, "FakeKeyISRTask", mainGENERIC_STACK_SIZE * 2,
 		    NULL, mainMAX_PRIORITY, &FakeKeyISRTask) != pdPASS) {
+	    goto err_demotask;
+    }    
+
+    if (xTaskCreate(vMyTask, "myTask_1", mainGENERIC_STACK_SIZE * 2,
+		    NULL, mainGENERIC_PRIORITY+1, &myTask) != pdPASS) {
+	    goto err_demotask;
+    }
+
+    if (xTaskCreate(vMyTask, "myTask_2", mainGENERIC_STACK_SIZE * 2,
+		    NULL, mainGENERIC_PRIORITY+1, &myTask2) != pdPASS) {
+	    goto err_demotask;
+    }
+
+    if (xTaskCreate(vPingPongTask, "vPingTask", mainGENERIC_STACK_SIZE * 2,
+		    "PING", mainGENERIC_PRIORITY+1, &pingTask) != pdPASS) {
+	    goto err_demotask;
+    }
+
+    if (xTaskCreate(vPingPongTask, "vPongTask", mainGENERIC_STACK_SIZE * 2,
+		    "PONG", mainGENERIC_PRIORITY+1, &pongTask) != pdPASS) {
 	    goto err_demotask;
     }
 
